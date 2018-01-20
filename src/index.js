2bb556d77f9080981884a3ebf9085fde0478f021
Alfonso <fernandez@ghinwa.me>
2018-01-20 19:23:14 +0200

    Hello Redux

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import initialState from './initialState';
+import configureStore from './configureStore'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = configureStore(initialState);
+store.dispatch({ type: 'HELLO_REDUX'})
+
+const ConnectedApp = () => (
+  <Provider store={store}>
+    <App />
+  </Provider >
+);
+
+ReactDOM.render(<ConnectedApp />, document.getElementById('root'));
 registerServiceWorker();
---
+++ src/initialState.js
@@ -0,0 +1,11 @@
+export default {
+  auth: {
+
+  },
+  database: {
+
+  },
+  view: {
+
+  }
+}
